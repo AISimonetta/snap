9d945b4392bda12bc963f73abde0337ec5e7b426
AISimonetta <info@simonettaalejandra.com>
2024-01-31 16:28:23 +0000

    instrucitons added and classes created

---
+++ src/CardGame.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class CardGame {
+//STAGE 1
+    //    Contains an ArrayList<Card> for the deckOfCards that contains all 52 cards. This is created and populated when the game is
+//    constructed.
+//    Has a name which is also defined in the constructor.
+//    Has a getDeck method that lists out the cards in the deck.
+
+    //STAGE 2
+//    Lets get some core functionality to our CardGame by implementing the following methods:
+//    Card dealCard()
+//    Takes the card from the top of the deck and returns it.
+//    ArrayList<Card> sortDeckInNumberOrder()
+//    Sorts the deck in number order (e.g. 2222333344445555 etc) and stores the new shuffled deck back into the deckOfCards attribute.
+//            ArrayList<Card> sortDeckIntoSuits()
+//    Sorts the deck into suits (2,3,4,5,6,7,8,9,10,J,Q,K,A of hearts, then 2,3,4,5,6,7,8,9,10,J,Q,K,A of clubs etc.) and stores the new shuffled
+//    deck back into the deckOfCards attribute.
+//    ArrayList<Card> shuffleDeck()
+//    Shuffles the deck into a random order and stores the new shuffled deck back into the deckOfCards attribute.
+}
---
+++ src/Snap.java
@@ -0,0 +1,11 @@
+public class Snap {
+//STAGE 3
+//    Create class for Snap that extends CardGame.
+//            This class should use the methods defined above, as well as some new ones, to enable the user to play the game snap according to the
+//    following rules:
+//    By pressing enter in the command line, the user takes their turn.
+//    Each turn, a new card is dealt from the deck.
+//    The game continues until two cards in a row have the same symbol, at which point the “player” wins and the game ends
+//}
+
+}
